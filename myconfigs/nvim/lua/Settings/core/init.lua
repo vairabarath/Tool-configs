9ed516daf90c0d65638bcdcc0a8391ee1d6307ef
Itachi Uchiha <139670785+vairabarath@users.noreply.github.com>
2025-08-01 08:57:06 +0530

    Delete myconfigs/nvim directory

--- myconfigs/nvim/lua/Settings/core/init.lua
+++
@@ -1,9 +0,0 @@
--- Load and setup all language-specific configurations
--- Note: Plugin-dependent configs are set up after plugins load via VeryLazy autocmd in lazy.lua
-local language_manager = require("Settings.languages")
-language_manager.setup_all()
-
--- Load core configurations
-require('Settings.core.options')
-require("Settings.core.keymaps")
-require("Settings.core.commands")
